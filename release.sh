7565f8a2a5ec047fc56b433f6e6ed451ed6d75e5
BryanAdamss <BryanAdamss@foxmail.com>
2019-12-04 11:03:46 +0800

    chore(release.sh): 调整发布脚本

--- release.sh
+++ release.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 set -e
 
+# 设置 npm源 为淘宝源
+setTbOrigin(){
+  echo "是否需要将npm 源设置为taobao源?(y/n)"
+
+  read CONFIRM
+
+  if [[ $CONFIRM == y ]]
+  then
+
+    echo "即将设置 npm源 为taobao源！"
+    npm config set registry https://registry.npm.taobao.org/
+
+    echo "设置 npm 为taobao源成功"
+    
+  fi
+}
+
+
 echo "输入发版类型：major | minor | patch"
 read TYPE
 echo "输入的为：$TYPE"
@@ -11,11 +29,13 @@ then
   echo "即将执行：npm run release -r $TYPE"
   npm run release -r $TYPE
 
-  echo "即将执行：npm run setNpmOrigin"
-  npm run setNpmOrigin
+  echo "即将执行：npm config set registry https://registry.npmjs.org/"
+  npm config set registry https://registry.npmjs.org/
 
-  echo "即将执行：npm run publish2Npm"
-  npm run publish2Npm
+  echo "即将执行：npm run publish2Remote"
+  npm run publish2Remote
+
+  setTbOrigin
 
   echo "发布成功"
   
